1d136e92f9af672cb21f0956da8baff0b4153540
Curry4182 <kangbk4182@naver.com>
2025-04-16 23:41:24 +0900

    first commit

---
+++ src/main/java/com/naribackend/NariBackendApplication.java
@@ -0,0 +1,13 @@
+package com.naribackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NariBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NariBackendApplication.class, args);
+    }
+
+}
