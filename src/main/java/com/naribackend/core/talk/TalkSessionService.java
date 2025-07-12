542aef4d4c9033298fa8ca324056ff50ad3b39a9
Curry4182 <kangbk4182@naver.com>
2025-07-12 14:28:14 +0900

    feat: session item 생성 메서드에 transactional 추가

--- src/main/java/com/naribackend/core/talk/TalkSessionService.java
+++ src/main/java/com/naribackend/core/talk/TalkSessionService.java
@@ -77,6 +77,7 @@ public class TalkSessionService {
     }
 
 
+    @Transactional
     public void createSessionItem(
             final LoginUser loginUser,
             final SessionItem sessionItem
