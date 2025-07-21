d72999cf86d44a09669e0c99ec1743eb937daef6
Curry4182 <kangbk4182@naver.com>
2025-07-21 22:47:47 +0900

    feat: 다이어리 목록조회 목 api 추가

---
+++ src/main/java/com/naribackend/api/v1/diary/DiaryController.java
@@ -0,0 +1,42 @@
+package com.naribackend.api.v1.diary;
+
+import com.naribackend.api.v1.diary.response.GetDiariesResponse;
+import com.naribackend.core.auth.CurrentUser;
+import com.naribackend.core.auth.LoginUser;
+import com.naribackend.core.diary.DiaryInfo;
+import com.naribackend.core.diary.DiaryService;
+import com.naribackend.support.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class DiaryController {
+
+    private final DiaryService diaryService;
+
+    @Operation(
+            summary = "다이어리 목록 조회",
+            description = "특정 연도와 월에 작성된 다이어리를 목록 조회합니다."
+    )
+    @GetMapping("/diaries")
+    public ApiResponse<?> getDiaries(
+            @Parameter(hidden = true) @CurrentUser LoginUser loginUser,
+            @RequestParam int year,
+            @RequestParam int month
+    ) {
+        List<DiaryInfo> diaryInfos = diaryService.getDiaryInfos(loginUser, year, month);
+
+        return ApiResponse.success(
+                GetDiariesResponse.from(diaryInfos)
+        );
+    }
+}
